0463f33096f4990a1c75738cf406ca041bf7bd92
Dima Osenkov <w3rgy1992@gmail.com>
2023-01-03 21:03:17 +0100

    js

--- app.js
+++ app.js
@@ -5,5 +5,38 @@ let pageSlider = new Swiper('.slider', {
 	scrollbar: {
 		el: ".slider__scrollbar",
 		draggable: true
+	},
+	breakpoints: {
+		"320": {
+			slidesPerView: 1,
+			centeredSlides: false
+		},
+		"992": {
+			slidesPerView: 2,
+			centeredSlides: true
+		}
 	}
 });
+
+const page = document.querySelector('.page');
+const images = document.querySelectorAll('.slide__picture');
+
+if (images.length) {
+	let backgroundSlides = ``;
+	let textSlides = ``;
+
+	images.forEach(image => {
+		backgroundSlides += `
+			<div class="background__slide swiper-slide">
+				<div class="background__image">
+					<img src="${image.getAttribute('src')}" alt="${image.alt}">
+				</div>
+			</div>
+		`;
+		textSlides += `
+			<div class="text__slide swiper-slide">
+				<span>${image.dataset.title ? image.dataset.title : ''}</span>
+			</div>
+		`;
+	});
+}
